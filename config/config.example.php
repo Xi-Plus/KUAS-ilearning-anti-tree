b5517ad640779228a07fe2191803823aca907ff8
xiplus <huangxuanyuxiplus@gmail.com>
2016-12-04 10:13:07 +0800

    finish~

---
+++ config/config.example.php
@@ -0,0 +1,8 @@
+<?php
+// rename this file to config.php and modify following data
+
+$cfg['username'] = 'your_user_id';
+$cfg['times'] = 1;
+$cfg['sleep'] = 1000;
+
+$cfg['headers'] = 'User-Agent:Mozilla/5.0（iPad; U; CPU OS 3_2_1 like Mac OS X; en-us）AppleWebKit/531.21.10（KHTML, like Gecko）Mobile/7B405';
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once("config/config.php");
+require_once("function/cURL-HTTP-function/curl.php");
+
+$header = array($cfg['headers']);
+
+$res = cURL_HTTP_Request("http://ilearning.kuas.edu.tw",null,$header);
+$html = $res->html;
+preg_match('/name="login_key" value="(.+?)"/', $html, $m);
+$login_key = $m[1];
+
+$post=array(
+	"username" => $cfg['username'],
+	"login_key" => $login_key
+);
+for ($i=1; $i <= $cfg['times']; $i++) { 
+	$res=cURL_HTTP_Request("http://ilearning.kuas.edu.tw/login.php",$post,$header);
+	echo "login ".$i."/".$cfg['times']."\n";
+	usleep($cfg['sleep']);
+}
