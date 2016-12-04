afd7bcff2d861b81c33c7debb9e44197f1ab3f97
xiplus <huangxuanyuxiplus@gmail.com>
2016-12-04 11:01:46 +0800

    headers

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 require_once("config/config.php");
 require_once("function/cURL-HTTP-function/curl.php");
 
-$header = array($cfg['headers']);
-
-$res = cURL_HTTP_Request("http://ilearning.kuas.edu.tw",null,$header);
+$res = cURL_HTTP_Request("http://ilearning.kuas.edu.tw",null,$cfg['headers']);
 $html = $res->html;
 preg_match('/name="login_key" value="(.+?)"/', $html, $m);
 $login_key = $m[1];
@@ -14,7 +12,7 @@ $post=array(
 	"login_key" => $login_key
 );
 for ($i=1; $i <= $cfg['times']; $i++) { 
-	$res=cURL_HTTP_Request("http://ilearning.kuas.edu.tw/login.php",$post,$header);
+	$res=cURL_HTTP_Request("http://ilearning.kuas.edu.tw/login.php",$post,$cfg['headers']);
 	echo "login ".$i."/".$cfg['times']."\n";
 	usleep($cfg['sleep']);
 }
